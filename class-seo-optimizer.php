0ae9c168ea33dddf561a9026c80c76e913111cac
vuvannamviet-sys <vuvannamviet@gmail.com>
2025-09-10 17:25:03 +0700

    Delete class-seo-optimizer.php

--- class-seo-optimizer.php
+++
@@ -1,9 +0,0 @@
-<?php
-if (!defined('ABSPATH')) exit;
-
-class GeminiAI_SEO_Optimizer {
-    public function analyze_content($content, $keywords) {
-        error_log('[GeminiAI_SEO_Optimizer] Analyze content for SEO...');
-        // Tính điểm SEO, kiểm tra từ khóa, tối ưu meta
-    }
-}
