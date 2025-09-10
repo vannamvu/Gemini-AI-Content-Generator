bf10334d7b9f2db09627864eb825f1b19b1a64a4
vuvannamviet-sys <vuvannamviet@gmail.com>
2025-09-10 17:14:38 +0700

    Delete includes/includes/image-processor.php

--- includes/includes/image-processor.php
+++
@@ -1,9 +0,0 @@
-<?php
-if (!defined('ABSPATH')) exit;
-
-class GeminiAI_Image_Processor {
-    public function optimize_image($image_path) {
-        error_log('[GeminiAI_Image_Processor] Optimize image: ' . $image_path);
-        // Convert to WebP, resize, nén ảnh
-    }
-}
